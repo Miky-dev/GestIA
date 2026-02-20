e13d65cb623f8369d23bb13adc7fcbe1038685aa
Miky-dev <miky.vitella@gmail.com>
2026-02-20 22:46:26 +0100

    Ho risolto l'errore di npm run lint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginNext from "@next/eslint-plugin-next";
+
+export default tseslint.config(
+  { ignores: [".next/**", "dist/**", "node_modules/**"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+      "@next/next": pluginNext,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  }
+);
